01d88f5aed35c53a14d647fc4e2702a81948a4ec
Maya-MD <maya.montanaro.c11325@mcast.edu.mt>
2021-01-24 16:18:59 +0100

    Bckground scroll & Destroy oranges
    Made the background scroll and loop.
    Made the oranges get destroyed when colliding with the character.
    Made enemy move from one side to the other.

---
+++ Assets/Scripts/backgroundloop.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundloop : MonoBehaviour
+{
+
+    public float moveSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * moveSpeed, Space.World);
+
+        //check if the platform went past  on y the axis
+        if(transform.position.y > 14.7f)
+        {
+            //teleport the platform to - on the axis
+            transform.position = new Vector3(transform.position.x, -28f,0);
+        }
+
+
+    }
+}
+
---
+++ Assets/Scripts/enemymove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemymove : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float speed;
+    public bool MoveRight;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MoveRight)
+        {
+            transform.Translate(2 * Time.deltaTime * speed, 0, 0);
+        }
+        else 
+        {
+            transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+        }
+
+        //check if the player went past 23.1f on x the axis
+        if(transform.position.x > 23.1f)
+        {
+            //teleport the player to -23.1f on the axis
+            transform.position = new Vector3(-23.1f,transform.position.y,0);
+        }
+
+        //check if player went past -23.1f on the x axis
+        else if (transform.position.x < -23.1f)
+        {
+            //teleport the player to 23.1f on the x axis
+            transform.position = new Vector3(23.1f,transform.position.y,0);
+        } 
+    }
+}
